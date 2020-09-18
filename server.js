327f2225c4fefa024bd98bb0ce0880fb75d52ae3
Calvin Lee <emailmecalvin@gmail.com>
2020-09-18 14:43:34 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+//code here
+// this app starts a server.
+const express = require("express");
+// middleware that allows easy log requests, errors and more to the console. 
+const logger = require("morgan");
+//ODM library modeling environment for data. 
+const mongoose = require("mongoose");
+// listens on port 3000 for connection. 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.Mongodb_uri || "mongodb://localhost/workout", { useNewURLParser: true });
+
+const db = require("./models");
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes"(app);
+
+app.listen(PORT, () => {
+    console.log(`app running on port ${PORT}`);
+});
+
+
